b601cbe4485c915997a200627e0843709fcbc470
Xenia Zagareva <xeniazagareva@gmail.com>
2023-11-12 23:55:25 +0300

    Almost ready tusk

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var lights: [UIView]!
+    @IBOutlet var switchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        switchButton.layer.cornerRadius = 10
+        lights.forEach{ $0.layer.cornerRadius = $0.bounds.width / 2}
+        lights.forEach{ $0.alpha = 0.3}
     }
 
-
+    @IBAction func tapButton(_ sender: Any) {
+        switchButton.setTitle("NEXT", for: .normal)
+        
+       
+    }
+    
 }
 
